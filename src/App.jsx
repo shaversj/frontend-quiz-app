bd3f63c5eb615339a0fd172f07fe348f10b01efe
Josh Shavers <shaversj@gmail.com>
2024-02-19 11:21:45 -0600

    Initial Commit + Tailwind Setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <>
+      <header></header>
+      <h1>Welcome to the Frontend Quiz!</h1>
+      <p>Pick a subject to get started</p>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Javascript</li>
+        <li>Accessibility</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      screens: {
+        sm: "375px",
+        lg: "1440px",
+      },
+      fontFamily: {
+        Rubik: ["Rubik", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
