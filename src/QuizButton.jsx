fc57d41526046f5f80225fda7e603a8dc58d3e2a
Josh Shavers <shaversj@gmail.com>
2024-03-05 17:08:09 -0600

    Add logic to support button select states

--- src/QuizButton.jsx
+++ src/QuizButton.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import Icon from "./Icon.jsx";
 
-const QuizButton = ({ icon, text, onClickHandler, idx, highlightedIndex, questionDispatch }) => {
+const QuizButton = ({ iconIndex, text, idx, highlightedIndex, questionDispatch }) => {
   const addSpaceBetweenColon = /(\d+(\.\d+)?)\:/g;
   return (
     <button
@@ -8,11 +8,13 @@ const QuizButton = ({ icon, text, onClickHandler, idx, highlightedIndex, questio
       onClick={() => questionDispatch({ type: "SELECT_ANSWER", idx })}
       className={
         highlightedIndex === idx
-          ? "flex w-full items-center gap-x-[16px] rounded-xl bg-pink-500 p-3 shadow-lg"
+          ? "flex w-full items-center gap-x-[16px] rounded-xl border-4 border-secondary-purple bg-white p-3 shadow-lg"
           : "flex w-full items-center gap-x-[16px] rounded-xl bg-white p-3 shadow-lg"
       }
     >
-      <span>{icon}</span>
+      <span>
+        <Icon iconIndex={iconIndex} classNames={""} highlightedIndex={highlightedIndex} />
+      </span>
       <h1 className={"text-left font-Rubik-Medium text-[18px] leading-6 text-dark-navy"}>{text.replace(addSpaceBetweenColon, "$1 : ")}</h1>
     </button>
   );
