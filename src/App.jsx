a5b32f9fb061a1e884d39228ced1cf3e0ec8843f
Josh Shavers <shaversj@gmail.com>
2024-03-16 09:30:11 -0500

    Add tablet and desktop layout to Homepage

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
   const [state, quizDispatch] = useReducer(reducer, initialState);
   return (
     <>
-      <div id={"app-container"} className={"font-Rubik h-[812px] max-w-screen-sm bg-light-grey"}>
+      <div id={"app-container"} className={"font-Rubik h-[812px] max-w-screen-sm bg-light-grey md:max-w-screen-md lg:max-w-screen-lg"}>
         {isQuizVisible ? (
           <>
             <Header iconPath={state.icon} text={state.title} />
--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -4,16 +4,16 @@ import { QUIZ_IMAGES } from "./constants.jsx";
 const HomePage = ({ quizDispatch }) => {
   return (
     <>
-      <main className={"pt-6"}>
-        <div className={"px-6"}>
-          <h1 className={"text-[40px] font-light leading-[2.82rem] text-dark-navy"}>
+      <main className={"px-6 pt-6 md:px-[64px] lg:flex lg:items-start lg:px-[140px] lg:pt-[99px]"}>
+        <div className={"lg:basis-1/2"}>
+          <h1 className={"text-[40px] font-light leading-[2.82rem] text-dark-navy md:text-[64px] md:leading-[64px]"}>
             Welcome to the <strong className={"font-medium"}>Frontend Quiz!</strong>
           </h1>
 
-          <p className={"pt-3 text-[14px] italic leading-none text-dark-navy"}>Pick a subject to get started</p>
+          <p className={"pt-3 font-Rubik-Italic text-[14px] leading-none text-grey-navy md:pt-4 md:text-[16px] lg:pt-[48px]"}>Pick a subject to get started.</p>
         </div>
 
-        <div className={"mx-6 space-y-3 pt-[40px]"}>
+        <div className={" space-y-3 pt-[40px] md:space-y-6 md:pt-[66px] lg:basis-1/2 lg:pt-0"}>
           {QUIZ_IMAGES?.data.map((quiz) => (
             <>
               <button onClick={() => quizDispatch({ type: "START_QUIZ", title: quiz.title })} className={"flex w-full items-center gap-x-[16px] rounded-lg bg-white p-3"}>
